e0ce05f8f5f62047bb7e19e0a7adc453bd0361eb
LJacobs <leo@the-earthworks.com>
2016-03-22 16:18:37 +0000

    Added animation from codrops

--- app.js
+++ app.js
@@ -17,16 +17,74 @@
      o.innerHTML = count;
      localStorage.setItem('count', count);
 
-          var burst = new mojs.Burst({
-            parent: el2,
-            duration: 200,
-            shape: 'circle',
-            fill: ['deeppink', 'cyan', 'orange'],
-            x: '50%',
-            y: '50%'
-          });
+           // Just a burst of circles    
+//          var burst = new mojs.Burst({
+//            parent: el2,
+//            duration: 200,
+//            shape: 'circle',
+//            fill: ['deeppink', 'cyan', 'orange'],
+//            x: '50%',
+//            y: '50%'
+//          });
+     
+     
+    var scaleCurve = mojs.easing.path('M0,100 L25,99.9999983 C26.2328835,75.0708847 19.7847843,0 100,0');
+    var el = document.querySelector('.icobutton'),
+	elSpan = el.querySelector('span'),
+	// mo.js timeline obj
+	timeline = new mojs.Timeline(),
+
+	// tweens for the animation:
+
+	// burst animation
+	tween1 = new mojs.Burst({
+		parent: el,
+		duration: 1500,
+		shape : 'circle',
+		fill : [ '#988ADE', '#DE8AA0', '#8AAEDE', '#8ADEAD', '#DEC58A', '#8AD1DE' ],
+		x: '50%',
+		y: '50%',
+		opacity: 0.6,
+		childOptions: { radius: {20:0} },
+		radius: {40:120},
+		count: 8,
+		isSwirl: true,
+		isRunLess: true,
+		easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
+	}),
+	// ring animation
+	tween2 = new mojs.Transit({
+		parent: el,
+		duration: 750,
+		type: 'circle',
+		radius: {0: 50},
+		fill: 'transparent',
+		stroke: '#988ADE',
+		strokeWidth: {15:0},
+		opacity: 0.6,
+		x: '50%',     
+		y: '50%',
+		isRunLess: true,
+		easing: mojs.easing.bezier(0, 1, 0.5, 1)
+	}),
+	// icon scale animation
+	tween3 = new mojs.Tween({
+		duration : 900,
+		onUpdate: function(progress) {
+			var scaleProgress = scaleCurve(progress);
+			elSpan.style.WebkitTransform = elSpan.style.transform = 'scale3d(' + scaleProgress + ',' + scaleProgress + ',1)';
+		}
+	});
+
+// add tweens to timeline:
+timeline.add(tween1, tween2, tween3);
+
+// when clicking the button start the timeline/animation:
+
+	timeline.start();
+
           
-     console.info(burst);
+    // console.info(burst);
    }
 
    btn.addEventListener("click", addMe);
